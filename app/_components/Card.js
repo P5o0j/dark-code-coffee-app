a8f7313f38ddfef3e0ff0e002b0d5294a8ce4775
Gerard Press <killerabs@gmail.com>
2025-01-25 18:07:12 +0000

    card created by map

--- app/_components/Card.js
+++ app/_components/Card.js
@@ -1,8 +1,8 @@
 import Image from "next/image";
 
-function Card() {
+function Card({ coffee }) {
   return (
-    <div className="bg-blue-900 w-full lg:w-[300] border p-2 rounded-lg hover:shadow-white hover:shadow-md">
+    <div className="bg-blue-900 lg:w-[350] border p-2 rounded-lg hover:shadow-white hover:shadow-md ">
       <Image
         src="/product_images/DDoSBag.jpg"
         width="175"
@@ -10,7 +10,9 @@ function Card() {
         alt="sample text"
         className="mx-auto my-3"
       />
-      <p className="text-center uppercase font-bold text-lg pb-3">title</p>
+      <p className="text-center uppercase font-bold text-lg pb-3">
+        {coffee.title}
+      </p>
       <hr />
       <p className="py-3">This is a short description</p>
       <div className="flex justify-end">
--- app/range/page.js
+++ app/range/page.js
@@ -1,73 +1,47 @@
+"use client";
+
 import Card from "../_components/Card";
+import { useState, useEffect } from "react";
+// import fakeCoffee from "../db.json";
 
-export const revalidate = 3600;
+// export const revalidate = 3600;
 
-export const metadata = {
-  title: "Our Range",
-};
+// export const metadata = {
+//   title: "Our Range",
+// };
 
 export default function Page() {
+  const [data, setData] = useState([]);
+
+  // Fetch the data from db.json
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("db.json"); // Adjust the path if the file is in a different location
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const jsonData = await response.json();
+        console.log(jsonData);
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+
+    // ;
+  }, []);
+
   return (
-    <div>
+    <>
       <h1 className="my-5">Our Range</h1>
-      <div className="flex flex-col items-center lg:flex-row lg:flex-wrap gap-5 justify-around px-6 lg:px-0">
-        <Card />
-        <div className="bg-blue-900 w-full lg:w-[300] border p-2 rounded-lg hover:shadow-white hover:shadow-md">
-          11111Gummi bears ice cream dragée jujubes powder soufflé. Gingerbread
-          macaroon bear claw bonbon macaroon sweet jelly beans. Icing macaroon
-          muffin donut muffin pudding sugar plum icing toffee. Biscuit chocolate
-          cake tart cake brownie jelly pastry cupcake chocolate. Dragée tiramisu
-          cake jelly-o dragée. Fruitcake oat cake donut macaroon lollipop bear
-          claw jujubes powder. Jujubes gummies apple pie chocolate sesame snaps
-          gingerbread powder muffin marshmallow. Sweet cookie dragée caramels
-          toffee sesame snaps oat cake cotton candy apple pie. Pie chocolate bar
-          chocolate cake caramels bear claw.
-        </div>
-        <div className="bg-blue-900 w-full lg:w-[300] border p-2">
-          222222Gummi bears ice cream dragée jujubes powder soufflé. Gingerbread
-          macaroon bear claw bonbon macaroon sweet jelly beans. Icing macaroon
-          muffin donut muffin pudding sugar plum icing toffee. Biscuit chocolate
-          cake tart cake brownie jelly pastry cupcake chocolate. Dragée tiramisu
-          cake jelly-o dragée. Fruitcake oat cake donut macaroon lollipop bear
-          claw jujubes powder. Jujubes gummies apple pie chocolate sesame snaps
-          gingerbread powder muffin marshmallow. Sweet cookie dragée caramels
-          toffee sesame snaps oat cake cotton candy apple pie. Pie chocolate bar
-          chocolate cake caramels bear claw.
-        </div>
-        <div className="bg-blue-900 w-full lg:w-[300] border p-2">
-          333333Gummi bears ice cream dragée jujubes powder soufflé. Gingerbread
-          macaroon bear claw bonbon macaroon sweet jelly beans. Icing macaroon
-          muffin donut muffin pudding sugar plum icing toffee. Biscuit chocolate
-          cake tart cake brownie jelly pastry cupcake chocolate. Dragée tiramisu
-          cake jelly-o dragée. Fruitcake oat cake donut macaroon lollipop bear
-          claw jujubes powder. Jujubes gummies apple pie chocolate sesame snaps
-          gingerbread powder muffin marshmallow. Sweet cookie dragée caramels
-          toffee sesame snaps oat cake cotton candy apple pie. Pie chocolate bar
-          chocolate cake caramels bear claw.
-        </div>
-        <div className="bg-blue-900 w-full lg:w-[300] border p-2">
-          44444Gummi bears ice cream dragée jujubes powder soufflé. Gingerbread
-          macaroon bear claw bonbon macaroon sweet jelly beans. Icing macaroon
-          muffin donut muffin pudding sugar plum icing toffee. Biscuit chocolate
-          cake tart cake brownie jelly pastry cupcake chocolate. Dragée tiramisu
-          cake jelly-o dragée. Fruitcake oat cake donut macaroon lollipop bear
-          claw jujubes powder. Jujubes gummies apple pie chocolate sesame snaps
-          gingerbread powder muffin marshmallow. Sweet cookie dragée caramels
-          toffee sesame snaps oat cake cotton candy apple pie. Pie chocolate bar
-          chocolate cake caramels bear claw.
-        </div>
-        <div className="bg-blue-900 w-full lg:w-[300] border p-2">
-          55555Gummi bears ice cream dragée jujubes powder soufflé. Gingerbread
-          macaroon bear claw bonbon macaroon sweet jelly beans. Icing macaroon
-          muffin donut muffin pudding sugar plum icing toffee. Biscuit chocolate
-          cake tart cake brownie jelly pastry cupcake chocolate. Dragée tiramisu
-          cake jelly-o dragée. Fruitcake oat cake donut macaroon lollipop bear
-          claw jujubes powder. Jujubes gummies apple pie chocolate sesame snaps
-          gingerbread powder muffin marshmallow. Sweet cookie dragée caramels
-          toffee sesame snaps oat cake cotton candy apple pie. Pie chocolate bar
-          chocolate cake caramels bear claw.
-        </div>
+      <div className="flex flex-col lg:flex-row gap-5 lg:justify-between px-5 lg:px-0">
+        {data.map((coffee) => (
+          <Card coffee={coffee} key={coffee.id} />
+        ))}
       </div>
-    </div>
+    </>
   );
 }
