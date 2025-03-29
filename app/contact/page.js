debb914e09c085b47f58ef7846157a6d1006f697
Gerard Press <killerabs@gmail.com>
2025-03-29 18:26:24 +0000

    Contact form styling

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,8 +1,48 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import Button from "../_components/Button";
+import Input from "../_components/Input";
+
 function Page() {
+  const router = useRouter();
   return (
-    <div>
-      <h1>Contact Us</h1>
-    </div>
+    <>
+      <form className="md:w-[70%] mx-auto bg-black p-7 rounded-md">
+        <h1>Contact Us</h1>
+        <Input label="Name:" id="name" type="name" name="name" required />
+        <Input label="Email:" id="email" type="email" name="email" required />
+        <Input
+          label="Order number:"
+          id="order"
+          type="text"
+          name="order"
+          placeholder="optional"
+        />
+
+        <div className="my-4 flex content-start">
+          <p className="w-[300]">
+            <label htmlFor="message" className="text-3xl mr-10 ">
+              Message:
+            </label>
+          </p>
+          <p>
+            <textarea
+              id="message"
+              className="w-[400] rounded-sm text-black"
+              rows="4"
+              cols="50"
+            ></textarea>
+          </p>
+        </div>
+
+        {/* BUTTONS */}
+        <div className="md:w-[60%] mx-auto flex justify-end py-6">
+          <Button type="reset">Reset</Button>
+          <Button>Send</Button>
+        </div>
+      </form>
+    </>
   );
 }
 
