4e0da8812a681e449a40a66cb053fa75b11f7d33
Gerard Press <killerabs@gmail.com>
2026-01-17 16:29:55 +0000

    main text start

--- app/page.js
+++ app/page.js
@@ -1,6 +1,49 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <div className={styles.page}>MAIN PAGE</div>;
+  return (
+    <main className={styles.page}>
+      <h1>Keep Calm and code on with DarkCode Coffee</h1>
+      <div>
+        <p>
+          Introducing DarkCode Coffee - the IT geek&apos;s best-kept secret for
+          fueling caffeine-powered coding marathons! This brew is the ultimate
+          hack for your taste buds, brewed with a proprietary blend of
+          high-octane beans that will keep your code running faster than a
+          quantum computer.
+        </p>
+        <p>
+          DarkCode Coffee is the go-to fuel for tech wizards, with its bold
+          flavor profile that&apos;s as smooth as a well-optimized algorithm.
+          With every sip, you&apos;ll feel the power of binary coursing through
+          your veins, igniting your coding skills and unlocking new levels of
+          productivity.
+        </p>
+      </div>
+      <div>
+        <p>
+          This coffee is brewed with an encrypted blend of dark roast beans that
+          are roasted to perfection by a team of master coffee hackers. The
+          result is a complex and robust flavor profile that will make your
+          taste buds sing in binary code.
+        </p>
+        <p>
+          With DarkCode Coffee, you&apos;ll never experience a system crash
+          during your coding sessions again. It&apos;s the coffee that keeps
+          your code running smoothly, eliminating bugs and glitches with every
+          sip. And the best part? It&apos;s compatible with all coding
+          languages, from Python to Java, making it the go-to choice for all
+          tech aficionados.
+        </p>
+      </div>
+      <div>
+        So, whether you&apos;re debugging lines of code or pushing the
+        boundaries of innovation, DarkCode Coffee is your trusty sidekick in the
+        world of IT. Say goodbye to lagging energy levels and hello to a coffee
+        experience that&apos;s as fast and furious as your coding skills. Get
+        ready to unlock the full potential of your inner tech ninja with
+        DarkCode Coffee!
+      </div>
+    </main>
+  );
 }
